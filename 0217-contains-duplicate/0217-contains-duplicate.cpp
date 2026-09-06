b6aa1b021536b849de404b06ee763b445e306211
Nitin-kumar9065 <nitin1.cs28@iilm.edu>
2026-09-06 21:59:54 +0530

    Time: 71 ms (60.68%), Space: 111.4 MB (46.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
+        unordered_set<int>mp;
         for(int i = 0; i < nums.size(); i++){
-            if(mp.find (nums[i]) != mp.end()){
+            if(mp.find(nums[i]) != mp.end()){
                 return true;
             }
-            mp[nums[i]] = i;
-            
-            
-           
+            mp.insert(nums[i]);    
         }
         return false;
     }
